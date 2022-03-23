b36c3dc8841089392669d81b0428483b7fc6f744
Samantha Navarro <samantha.m.navarro@gmail.com>
2022-03-22 20:32:04 -0700

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,9 @@
+const h2 = document.createElement("h2");
+h2.textContent = "Let's get Creative";
+
+document.querySelector("body").appendChild(h2);
+
+const h4 = document.createElement("h4");
+h4.textContent = "This is going to be a lot of fun.";
+
+document.querySelector("body").appendChild(h4);
